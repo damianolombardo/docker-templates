07b58918ed2baeac75a6b7700e96edb80c3d02f3
Damiano Lombardo <lombardo.damiano@gmail.com>
2021-10-06 07:08:01 +0800

    set defaults for anaconda as arguments

--- damianolombardo/anaconda3-entrypoint.sh
+++ damianolombardo/anaconda3-entrypoint.sh
@@ -2,4 +2,4 @@
 conda config --prepend pkgs_dirs $CONDA_CACHE_DIR 
 conda install jupyter $CONDA_PKGS -y 
 pip install --no-input $PIP_PKGS 
-jupyter $NOTEBOOK_OR_LAB
+jupyter $NOTEBOOK_OR_LAB --allow-root --no-browser --ip=$IP --notebook-dir=/opt/src
