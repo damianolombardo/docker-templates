c0b68258b78640ff421e96e61c8ad08ce18b6151
Damiano Lombardo <lombardo.damiano@gmail.com>
2023-05-07 20:10:22 +0800

    Update anaconda3-entrypoint.sh

--- damianolombardo/anaconda3-entrypoint.sh
+++ damianolombardo/anaconda3-entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 conda config --prepend pkgs_dirs $CONDA_CACHE_DIR 
+if [ -n "${DEFAULT_CONDA_ENV}" ]; then conda activate $DEFAULT_CONDA_ENV; fi
 conda install --file /opt/data/conda.txt -y 
 pip install --no-input -r /opt/data/pip.txt 
 jupyter $NOTEBOOK_OR_LAB --allow-root --no-browser --ip=$IP --notebook-dir=/opt/src
