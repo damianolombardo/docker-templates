0f8774de400243bb6658f7190c9f9548054dce6e
Damiano Lombardo <lombardo.damiano@gmail.com>
2025-06-17 03:50:21 +0800

    create env

--- damianolombardo/anaconda3-entrypoint.sh
+++ damianolombardo/anaconda3-entrypoint.sh
@@ -4,7 +4,13 @@ conda init bash
 source ~/.bashrc
 
 conda config --prepend pkgs_dirs $CONDA_CACHE_DIR 
-if [ -n "${DEFAULT_CONDA_ENV}" ]; then conda activate $DEFAULT_CONDA_ENV; fi
+if [ -n "${DEFAULT_CONDA_ENV}" ]; then 
+    if ! conda env list | grep -q "^${DEFAULT_CONDA_ENV}\s"; then
+        echo "Creating conda environment: ${DEFAULT_CONDA_ENV}"
+        conda create -n $DEFAULT_CONDA_ENV -y
+    fi
+    conda activate $DEFAULT_CONDA_ENV
+fi
 conda install --file /opt/data/conda.txt -y 
 pip install --no-input -r /opt/data/pip.txt 
 jupyter $NOTEBOOK_OR_LAB --allow-root --no-browser --ip=$IP --notebook-dir=/opt/src
