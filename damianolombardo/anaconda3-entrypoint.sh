022bc5ed76dea16fb0290361266529f562f37da7
Damiano Lombardo <lombardo.damiano@gmail.com>
2025-06-17 15:03:31 +0800

    password

--- damianolombardo/anaconda3-entrypoint.sh
+++ damianolombardo/anaconda3-entrypoint.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
+# Initialize conda for bash shell
 conda init bash
 source ~/.bashrc
 
 conda config --prepend pkgs_dirs $CONDA_CACHE_DIR 
-if [ -n "${DEFAULT_CONDA_ENV}" ]; then 
-    if ! conda env list | grep -q "^${DEFAULT_CONDA_ENV}\s"; then
-        echo "Creating conda environment: ${DEFAULT_CONDA_ENV}"
-        conda create -n $DEFAULT_CONDA_ENV -y
-    fi
-    conda activate $DEFAULT_CONDA_ENV
-fi
 conda install --file /opt/data/conda.txt -y 
 pip install --no-input -r /opt/data/pip.txt 
-jupyter $NOTEBOOK_OR_LAB --allow-root --no-browser --ip=$IP --notebook-dir=/opt/src
+
+# Set up Jupyter password if provided
+JUPYTER_ARGS="--allow-root --no-browser --ip=$IP --notebook-dir=/opt/src"
+if [ -n "${JUPYTER_PASSWORD}" ]; then
+    # Generate password hash and set it
+    JUPYTER_PASSWORD_HASH=$(python -c "from jupyter_server.auth import passwd; print(passwd('${JUPYTER_PASSWORD}'))")
+    JUPYTER_ARGS="$JUPYTER_ARGS --ServerApp.password='$JUPYTER_PASSWORD_HASH'"
+fi
+
+jupyter $NOTEBOOK_OR_LAB $JUPYTER_ARGS
